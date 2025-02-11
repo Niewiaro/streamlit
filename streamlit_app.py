2b6862f9ffa15fa7a4942f9a4a9a3defee6c9389
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-02-11 14:30:26 +0100

    feat: navigation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
-st.title("Streamlit init!")
+home_page = st.Page(
+    page="views/home.py",
+    title="Home",
+    icon=":material/home:",
+    default=True,
+)
+
+about_page = st.Page(
+    page="views/about.py",
+    title="About",
+    icon=":material/info:",
+)
+
+flashcards_page = st.Page(
+    page="views/flashcards.py",
+    title="Flashcards",
+    icon=":material/playing_cards:",
+)
+
+pg = st.navigation(pages=[home_page, about_page, flashcards_page])
+
+pg.run()
