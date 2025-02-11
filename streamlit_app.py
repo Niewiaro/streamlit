3ca6c0a3086ccaf4adccd07844dc1b079afa91b5
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-02-11 14:36:32 +0100

    style: nav sections

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,12 @@ flashcards_page = st.Page(
     icon=":material/playing_cards:",
 )
 
-pg = st.navigation(pages=[home_page, about_page, flashcards_page])
+# pg = st.navigation(pages=[home_page, about_page, flashcards_page])
+pg = st.navigation(
+    {
+        "Main": [home_page, about_page],
+        "Projects": [flashcards_page],
+    }
+)
 
 pg.run()
